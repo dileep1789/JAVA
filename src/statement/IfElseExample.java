2214a7ec341ffb00c1d368072d419420d61af7a9
Dileep Kumar <dileepkrmuz1121@gmail.com>
2026-03-21 16:26:02 +0530

    Merged unrelated histories

---
+++ src/statement/IfElseExample.java
@@ -0,0 +1,13 @@
+package statement;
+
+public class IfElseExample {
+    public static void main(String[] args) {
+        int num = -5;
+
+        if (num > 0) {
+            System.out.println("Positive");
+        } else {
+            System.out.println("Negative");
+        }
+    }
+}
