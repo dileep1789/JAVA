512c8da1c33c0d9288661f7997f298e513318cc2
Dileep Kumar <dileepkrmuz1121@gmail.com>
2026-03-21 15:11:59 +0530

    jav

---
+++ src/DataTypesExample.java
@@ -0,0 +1,30 @@
+public class DataTypesExample {
+    public static void main(String[] args) {
+
+        // Integer types
+        byte b = 10;
+        short s = 200;
+        int i = 5000;
+        long l = 100000L;
+
+        // Floating point types
+        float f = 10.5f;
+        double d = 20.99;
+
+        // Character type
+        char c = 'A';
+
+        // Boolean type
+        boolean isJavaFun = true;
+
+        // Printing values
+        System.out.println("Byte value: " + b);
+        System.out.println("Short value: " + s);
+        System.out.println("Int value: " + i);
+        System.out.println("Long value: " + l);
+        System.out.println("Float value: " + f);
+        System.out.println("Double value: " + d);
+        System.out.println("Char value: " + c);
+        System.out.println("Boolean value: " + isJavaFun);
+    }
+}
