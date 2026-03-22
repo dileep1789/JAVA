df4d592c6eef55e2c3a54b3994fecebd979e6b13
Dileep Kumar <dileepkrmuz1121@gmail.com>
2026-03-22 11:49:16 +0530

    Merged unrelated histories

---
+++ src/function.java
@@ -0,0 +1,11 @@
+
+class Tfunction {
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int sum = add(10, 20);
+        System.out.println(sum);
+    }
+}
