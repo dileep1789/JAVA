fed7c310898a5e61c20d15a22bc6c9916894ff71
Dileep Kumar <dileepkrmuz1121@gmail.com>
2026-03-22 11:34:38 +0530

    Merged unrelated histories

---
+++ src/statement/ifels_else.java
@@ -0,0 +1,17 @@
+package statement;
+
+public class ifels_else {
+    public static void main(String[] args) {
+
+
+        int marks = 75;
+
+        if (marks >= 90) {
+            System.out.println("Grade A");
+        } else if (marks >= 70) {
+            System.out.println("Grade B");
+        } else {
+            System.out.println("Grade C");
+        }
+    }
+}
